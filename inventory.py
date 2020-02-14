958dead001810629102ceb5946530e0e9ba834fa
Maksim Sharoika <maxim.sharoika@rbe.sk.ca>
2020-02-14 13:37:36 -0600

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -4,18 +4,25 @@
 # Date last modified: 13/02/20
 # Name: Maksim Sharoika
 # Description: Inventory Test Program
+##########
+# Creating the lists for the inventory.
 consumables = []
 weapons = []
-
+##########
+# Seperation line
 print('--------------------')
+##########
+# Saying the items that are in the inventory at start.
 print(
     'Currents weapons : ' + f'{weapons}'
     f'\n'
     'Current consumables : ' + f'{consumables}'
 )
 ##########
+# Seperation line
 print('--------------------')
 ##########
+# Appending and inserting weapons and consumables into inventory.
 print('1. Weapons and consumables picked up!')
 weapons.append('knife')
 weapons.append('rifle')
@@ -28,8 +35,10 @@ print(
     'Current consumables : ' + f'{consumables}'
 )
 ##########
+# Seperation line
 print('--------------------')
 ##########
+# Removing an item from the list using the pop() command.
 print('2. You eat some food because you are hungry!')
 consumables.pop()
 print(
@@ -38,8 +47,10 @@ print(
     'Current consumables : ' + f'{consumables}'
 )
 ##########
+# Seperation line
 print('--------------------')
 ##########
+# Removing an item from the list using the del command.
 print('3. You relize you dont have any bullets and chuck the gun!')
 del weapons[1]
 print(
@@ -48,8 +59,10 @@ print(
     'Current consumables : ' + f'{consumables}'
 )
 ##########
+# Seperation line
 print('--------------------')
 ##########
+# Removing an item from the list using the remove command, adding via append.
 print('4. You stab someone, loose knife gain chunk of flesh')
 weapons.remove('knife')
 consumables.append('flesh')
@@ -59,8 +72,10 @@ print(
     'Current consumables : ' + f'{consumables}'
 )
 ##########
+# Seperation line
 print('--------------------')
 ##########
+# Sorting the items in the correct order.
 print('5. You take a break, and decide to sort your things!')
 consumables.sort()
 print(
@@ -69,8 +84,10 @@ print(
     'Current consumables : ' + f'{consumables}'
 )
 ##########
+# Seperation line
 print('--------------------')
 ##########
+# Sorting the items in the reverse order via sorted command.
 print('6. You dont like that way, so you sort them backwards!')
 print(
     'Currents weapons : ' + f'{weapons}'
@@ -78,8 +95,10 @@ print(
     'Current consumables : ' + f'{sorted(consumables, reverse = True)}'
 )
 ##########
+# Seperation line
 print('--------------------')
 ##########
+# Sorting the items in the reverse order via reverse command.
 print('7. You like it that way so you keep it like that!')
 consumables.reverse()
 print(
@@ -88,10 +107,12 @@ print(
     'Current consumables : ' + f'{consumables}'
 )
 ##########
+# Seperation line
+# Using the len() command to say the number in the list.
 print('--------------------')
 ##########
 print('8. A man asks you how many items you are carrying!')
-print('You say: I have ' + f'{len(consumables)}' + ' items.')
+print('You say: I have ' + f'{len(consumables) + len(weapons)}' + ' items.')
 ##########
 print('--------------------')
 ##########
